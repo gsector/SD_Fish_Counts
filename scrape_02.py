b55463ff6179de5abda0876f4abf44d8806ee0bf
Stephen <stephen@gsector.net>
2016-10-22 19:09:55 -0700

    database hooked up!

--- scrape_02.py
+++ scrape_02.py
@@ -3,21 +3,46 @@ import bs4
 import re
 import datetime
 import tqdm
+import sqlite3
 
+# Function to prep string for CSV output
 def csvPrep(t):
     t = t.strip()
     t = t.replace('"','""')
     return '"' + t + '"'
 
+# Create Insert query
+def insertQuery(d):
+    dbFields = 'Date,Landing,Boat,Trip,Anglers,Fish,Species'
+    tableName = 'Scraped_Data'
+    q = ''
+    q += '\'' + d["date"].replace('\'','') + '\'' + ','
+    q += '\'' + d["landing"].replace('\'','') + '\'' + ','
+    q += '\'' + d["boat"].replace('\'','') + '\'' + ','
+    q += '\'' + d["tripType"].replace('\'','') + '\'' + ','
+    q += '\'' + d["anglers"].replace('\'','') + '\'' + ','
+    q += '\'' + d["numFish"].replace('\'','') + '\'' + ','
+    q += '\'' + d["species"].replace('\'','') + '\''
+    
+    query = 'INSERT OR REPLACE INTO {table} ({fields}) VALUES ({values});'.format(table=tableName,fields=dbFields,values=q)
+    return query
+
+
 # Variables/Parameters
 url = 'http://sandiego.fishreports.com/dock_totals/boats.php?date='
 currentDate = datetime.date.today()
 lastDate = datetime.date(2000,1,1)
+# lastDate = datetime.date(2016,10,20) # Entry for testing only
 numDays = int((currentDate - lastDate).days)
 print(str(numDays))
-# Open File to write to
-f = open('output.csv','a')
 
+# Open File/Database to write to
+    # f = open('output.csv','a')
+conn = sqlite3.connect('Fish_Counts.db')
+c = conn.cursor()
+c.execute('BEGIN TRANSACTION;')
+
+# Loop through every day
 for dayz in tqdm.tqdm(range(numDays)):    # Get Request Results
     queryDate = str(currentDate - datetime.timedelta(days=dayz))
     queryUrl = 'http://sandiego.fishreports.com/dock_totals/boats.php?date=' + queryDate
@@ -85,14 +110,23 @@ for dayz in tqdm.tqdm(range(numDays)):    # Get Request Results
             rexp = '(\D*)'
             tripData['species'] = fish.replace(tripData['numFish'],'').strip()
             
-            csvString = csvPrep(tripData['date']) \
-                        + ',' + csvPrep(tripData['landing']) \
-                        + ',' + csvPrep(tripData['boat']) \
-                        + ',' + csvPrep(tripData['tripType']) \
-                        + ',' + csvPrep(tripData['anglers']) \
-                        + ',' + csvPrep(tripData['numFish']) \
-                        + ',' + csvPrep(tripData['species']) \
-                        + '\n'
-            f.write(csvString)
 
-f.close()
+            # Write data to csv file
+            # csvString = csvPrep(tripData['date']) \
+                        #+ ',' + csvPrep(tripData['landing']) \
+                        #+ ',' + csvPrep(tripData['boat']) \
+                        #+ ',' + csvPrep(tripData['tripType']) \
+                        #+ ',' + csvPrep(tripData['anglers']) \
+                        #+ ',' + csvPrep(tripData['numFish']) \
+                        #+ ',' + csvPrep(tripData['species']) \
+                        #+ '\n'
+            # f.write(csvString)
+
+            # Write data to DB
+            dbQuery = insertQuery(tripData)
+            c.execute(str(dbQuery))
+
+# Close connections
+# f.close() # Close CSV File
+conn.commit()
+conn.close()
